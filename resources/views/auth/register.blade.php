dd242c441f526d6524ece27ff35b545fdd58a208
Ramiro <ramiroavch@gmail.com>
2018-12-20 11:50:30 -0400

    Arreglando Registrar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,10 +38,17 @@
                         <div class="form-group">
                                 <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required>
                         </div>
-                        <button type="submit" class="btn btn-primary block full-width m-b">
-                                    {{ __('Register') }}
-                        </button>
-                        <p class="text-muted text-center"><small>Already have an account?</small></p>
-                        <a class="btn btn-sm btn-white btn-block" href="/login">Login</a>
+                        <div class="row">
+                            <button type="submit" class="btn btn-primary block col-md-5">
+                                        {{ __('Registrar') }}
+                            </button>
+                            <div class="col-md-2">
+                            </div>
+                            <a class="btn btn-danger col-md-5" href="{{ route('home') }}">
+                                        {{ __('Cancelar') }}
+                            </a>
+
+                        </div>
+                        
                     </form>
 @endsection
